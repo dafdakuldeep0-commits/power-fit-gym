c64ae8d6bccb7e94a479dfac02dc19e7a7b057a1
Dafda kuldeep <dafdakuldeep0@gmail.com>
2026-01-27 20:47:09 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Join button click
+const joinBtn = document.querySelector(".btn");
+
+if (joinBtn) {
+  joinBtn.addEventListener("click", () => {
+    alert("Welcome to Power Fit Gym 💪");
+  });
+}
